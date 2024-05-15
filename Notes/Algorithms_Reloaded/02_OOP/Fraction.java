b262e90d2d15fa4b497d6b46b088ae3bf4dc75ca
Brandon Fortes <55511864+MrFortez@users.noreply.github.com>
2024-05-15 15:08:57 -0500

    5/15/2024

---
+++ Notes/Algorithms_Reloaded/02_OOP/Fraction.java
@@ -0,0 +1,25 @@
+public class Fraction {
+
+    private int numerator;
+    private int denominator;
+
+    public Fraction() {
+
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public void setNumerator(int value) {
+        numerator = value;
+    }
+
+    public void setDenominator(int value) {
+        denominator = value;
+    }
+}
---
+++ Notes/Algorithms_Reloaded/02_OOP/Main.java
@@ -0,0 +1,104 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+      System.out.println("Hello World");
+      System.out.println("poggers");
+
+      for (int i = 0; i < args.length; i++) {
+        System.out.println(args[i]);
+      }
+
+      Fraction fraction = new Fraction(4, 20);
+      System.out.println(fraction);
+      System.out.println(fraction.toString());
+
+      Fraction f3 = new Fraction(3,4);
+
+      f3 = fraction.add(f3);
+      System.out.println(f3);
+
+    }
+  }
+
+class Fraction {
+
+    private int numerator;
+    private int denominator;
+
+    public Fraction(int numerator, int denominator) {
+        setNumerator(numerator);
+        setDenominator(denominator);
+        reduce();
+    }
+
+    public Fraction() {
+        setNumerator(0);
+        setDenominator(1);
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public void setNumerator(int value) {
+        numerator = value;
+        reduce();
+    }
+
+    public void setDenominator(int value) {
+        if (value != 0) {
+            denominator = value;
+            reduce();
+        }
+        else if (denominator == 0) {
+            denominator = 1;
+        }
+    }
+
+    private void reduce() {
+        int gcd = 1;
+        int min = Math.min(Math.abs(numerator),Math.abs(denominator));
+
+        for(int i = 2; i<=min; i++)
+            if (numerator % i == 0 && denominator % i == 0 ){
+                gcd = i;    
+            }
+
+        numerator = numerator / gcd;
+        denominator /= gcd;
+
+        if (numerator==0){
+            denominator = 1;
+        }
+    }
+
+    //float - a value represented in memory with 32 bits
+    //        uses IEEE 754 to represent the value
+    //double - a value reprsented in memory with 64 bits
+    //        uses IEEE 754 to represent the value
+    private double getReal(){
+        //casting is done using parentheses (type)
+        //all literal decimals in java are doubles
+        //float value = 6.789;
+        return (double) numerator / denominator;
+
+    }
+
+    public Fraction add(Fraction other){
+        Fraction sum = new Fraction();
+        sum.numerator = numerator * other.denominator + denominator * other.numerator;
+        sum.denominator = denominator * other.denominator;
+        sum.reduce();
+        return sum;
+    }
+    //use toString() to similar to __str__ in python
+    public String toString(){
+        return numerator + "/" + denominator + " (" + getReal() + ")";
+    }
+
+}
